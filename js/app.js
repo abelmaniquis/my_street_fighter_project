fdb27996ff0a551454926eca12983ca0cd32dcad
abelmaniquis <abelmaniquis@gmail.com>
2016-04-13 20:29:06 +0000

    first version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-  $('.ryu').mouseenter(function() {
+    $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-  .mouseleave(function(){
+  $('.ryu').mouseleave(function(){
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
   .mousedown(function(){
     playHadouken();
     $('.ryu-ready').hide();
-    $('ryu-throwing').show();
+    $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
-      {'left':'1020px'},
-      500,
+      {'left':'1020px'}, //'left':'1020px'
+      500, /*time in milliseconds*/
       function(){
         $(this).hide();
         $(this).css('left','520px')
@@ -23,11 +23,29 @@ $(document).ready(function(){
   .mouseup(function(){
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-    //ryu goes back to his ready position.
   });
+  /*do the ryu cool pose*/
 });
+
+$(document).keydown(function(e){
+    if(e.keyCode == 88){
+    $('.ryu').hide();
+    $('.ryu-cool').show();
+    }
+  }).keyup(function(){
+    $('.ryu-cool').hide();
+    $('.ryu').show();
+  });
+  
 function playHadouken(){
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
+
+
+
+
+
+
+/*you can refactor this code */
